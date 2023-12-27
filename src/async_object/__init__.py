3541842068d2877569e7c274e0b4133d38f971c2
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2023-12-27 14:19:35 +0100

    [RELEASE] v2.0.0

--- src/async_object/__init__.py
+++ src/async_object/__init__.py
@@ -20,7 +20,7 @@ __email__ = "clairicia.rcj.francis@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "FrankySnow9"
 __status__ = "Production"
-__version__ = "1.1.1"
+__version__ = "2.0.0"
 
 import abc
 import inspect
